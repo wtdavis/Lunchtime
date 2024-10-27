d1123cd62cf0395a2f866361ed4b941b78c4552f
Will <wtdavis@wesleyan.edu>
2024-10-26 22:02:54 -0400

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
+import "./app.css"
 import Header from "./Header/Header.jsx"
 import TileList from "./TileList/TileList.jsx"
-import "./app.css"
 
 function App () {
 
