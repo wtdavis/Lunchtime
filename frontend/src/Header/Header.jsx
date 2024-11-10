5bf0c5ff8ae4fb1209414c8772461cd1b32e77c1
Will <wtdavis@wesleyan.edu>
2024-11-09 21:30:17 -0500

    test

--- frontend/src/Header/Header.jsx
+++ frontend/src/Header/Header.jsx
@@ -18,6 +18,7 @@ function Header () {
                 <SearchBar/>
                 <SearchBar/>
                 <SearchBar/>
+                <SearchBar/>
 
                 <SearchBar/>
                 <SearchBar/>
