61423f0042d270ee4785a9c2025843de02fb27d8
Will <wtdavis@wesleyan.edu>
2024-11-16 23:33:37 -0500

    test

--- frontend/src/StudentTile/StudentTile.jsx
+++ frontend/src/StudentTile/StudentTile.jsx
@@ -5,14 +5,14 @@ function StudentTile (name, key) {
     const [toggleStatus, setToggleStatus] = useState("student-tile-toggle")
 
     function handleToggle () {
-
+        if (toggleStatus==)
     }
 
     return (
         <>
             <div className="student-tile student-name-tile" key={key}>
                 <p className="student-tile-name student-tile-element">This is {name}'s student tile</p>
-                <div className="button student-tile-toggle student-tile-element">button
+                <div className="button student-tile-toggle student-tile-element" onClick={handleToggle}>button
                     <div className=" button student-tile-toggle-indicator"></div>
                 </div>
             </div>
