f5379f11678c44d7927bde9ecef6913e9f81de66
wtdavis <wtdavis@wesleyan.edu>
2024-12-06 00:16:35 -0500

    fix express db integration

--- db.js
+++ db.js
@@ -2,5 +2,13 @@ const mysql = require('mysql')
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    
-})
+    user: 'lunchtime',
+    password: 'lunchtime',
+    database: 'lunchtime'
+});
+
+connection.connect();
+
+connection.query();
+
+connection.end();
