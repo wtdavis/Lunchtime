753e63082c25db919476a1354fdead70e231e12f
wtdavis <wtdavis@wesleyan.edu>
2024-12-09 22:32:22 -0500

    serpsasdf

--- dbSetup.sql
+++ dbSetup.sql
@@ -1,9 +1,18 @@
 DROP DATABASE lunchtime;
 CREATE DATABASE lunchtime;
 USE lunchtime;
+DROP TABLE students;
 CREATE TABLE students (
     id primary key,
     studentFirstName varchar(255),
     studentLastName varchar(255),
     studentSocialist boolean
-)
+);
+INSERT INTO students (
+    studentFirstName, studentLastName, studentSocialist
+) VALUES (
+    ('john', 'jacob', 0),
+    ('sarah', 'jenkins', 1),
+    ('steven', 'evens', 1),
+    ('evelynda', 'evangelista', 0)
+);
