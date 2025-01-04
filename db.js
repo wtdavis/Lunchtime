6faec3f6e09c01280ee854e515ecb1bae4932828
Will <wtdavis@wesleyan.edu>
2025-01-04 18:46:39 -0500

    derp

--- db.js
+++ db.js
@@ -9,9 +9,10 @@ const lunchtimeConnectionObj = {
 
 export function dbUserSetup () {
     const connection = mysql.createConnection(lunchtimeConnectionObj)
+    
     connection.connect();
     connection.query(`CREATE TABLE users (id index, username varchar(255), hash varchar(255), admin boolean`)
-    connection.query(`INSERT INTO users (username, admin) VALUES ('${process.env.admin}', 1)`)
+    connection.query(`INSERT INTO users (username, hash, admin) VALUES ('${process.env.admin}', 1)`)
 }  
 
 export function dbStudentsSetup () {
